f8b4a0d946ba6622006ff73d4b0e7fb7ee48bd1f
jhanca-vm <jhanca-vm@proton.me>
2023-04-25 18:18:14 -0500

    Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config'
+import cloudflare from '@astrojs/cloudflare'
 
 export default defineConfig({
+  adapter: cloudflare(),
+  output: 'server',
   server: { host: true }
 })
