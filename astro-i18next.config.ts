1521c1fbd462e8b43c7bed2afd7cdf8619ef9ec8
jhanca-vm <jhanca-vm@proton.me>
2023-05-25 16:23:19 -0500

    Add i18n

---
+++ astro-i18next.config.ts
@@ -0,0 +1,8 @@
+import type { AstroI18nextConfig } from 'astro-i18next'
+
+const config: AstroI18nextConfig = {
+  defaultLocale: 'es',
+  locales: ['es', 'en']
+}
+
+export default config
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config'
+import i18next from 'astro-i18next'
 
 export default defineConfig({
-  server: { host: true }
+  server: { host: true },
+  integrations: [i18next()]
 })
