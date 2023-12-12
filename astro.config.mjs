b2df1c2a03b17c602c5e85e1bc19a3620271d737
jhanca-vm <jhanca-vm@proton.me>
2023-12-12 14:11:10 -0500

    chore: Update design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config'
+import svelte from '@astrojs/svelte'
 import unocss from 'unocss/astro'
 
 export default defineConfig({
   server: { host: true, port: 3000 },
-  integrations: [unocss()]
+  integrations: [svelte(), unocss()]
 })
--- uno.config.ts
+++ uno.config.ts
@@ -1,16 +1,25 @@
-import { defineConfig, transformerVariantGroup } from 'unocss'
+import {
+  defineConfig,
+  transformerDirectives,
+  transformerVariantGroup
+} from 'unocss'
 
 export default defineConfig({
   theme: {
     colors: {
-      default: {
-        100: '#ffffff',
-        200: '#d9d9d9',
-        300: '#242424',
-        400: '#151515'
-      },
-      primary: '#4ee1a0'
+      primary: '#4ee1a0',
+      secondary: '#242424',
+      accent: '#d9d9d9',
+      neutral: '#151515',
+      error: '#ff6f5b'
+    },
+    fontSize: {
+      '2xl': ['1.5rem', '2rem']
+    },
+    letterSpacing: {
+      tighter: '-0.0284em',
+      tight: '-0.013875em'
     }
   },
-  transformers: [transformerVariantGroup()]
+  transformers: [transformerDirectives(), transformerVariantGroup()]
 })
