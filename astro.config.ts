647401325cc7a95492d13a141f3ac751d55a8c84
jhanca-vm <jhanca-vm@proton.me>
2025-06-06 10:32:43 -0500

    feat: update design

--- astro.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  vite: { plugins: [tailwindcss()] },
-  server: { host: true, port: 3000 },
-  devToolbar: { enabled: false }
-})
