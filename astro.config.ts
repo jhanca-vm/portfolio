fad22f91346d499aa9c7617447b47986bb5b37f5
jhanca-vm <jhanca-vm@proton.me>
2026-06-11 08:43:59 -0500

    feat: upgrade design and dependencies

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig, fontProviders } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
   server: { host: true, port: 3000 },
-  devToolbar: { enabled: false }
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Be Vietnam Pro',
+      cssVariable: '--font-be-vietnam-pro',
+      weights: ['400', '500', '600'],
+      styles: ['normal']
+    }
+  ]
 })
